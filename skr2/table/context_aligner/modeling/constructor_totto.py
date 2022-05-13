4193ab6516445cf1d6dc114049afe076f16c8944
Peng Shi <spone.2014@gmail.com>
2022-05-13 14:48:11 -0400

    Add context aligner training code

---
+++ skr2/table/context_aligner/modeling/constructor_totto.py
@@ -0,0 +1,105 @@
+import copy
+import os
+from copy import deepcopy
+
+import torch
+from datasets import DatasetDict
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
+from transformers import AutoTokenizer
+from tqdm import tqdm
+
+label_id2label_str = {
+    1: "matched",
+    0: "mismatched"
+}
+
+
+class Constructor(object):
+    def __init__(self, args):
+        self.args = args
+
+    def to_seq2seq(self, raw_datasets: DatasetDict, cache_root: str):
+        if not len(raw_datasets) == 2:
+            raise AssertionError("Train, Dev, Test sections of dataset expected.")
+        train_dataset = TrainDataset(self.args, raw_datasets['train'], cache_root)
+        dev_dataset = DevDataset(self.args, raw_datasets['validation'], cache_root)
+
+        return train_dataset, dev_dataset
+
+
+"""
+    Raw data are formatted as:
+    {
+        "id": datasets.Value("string"),
+        "statement": datasets.Value("string"),
+        "table": datasets.Value("string"),
+        "label": datasets.Value("int32"),
+    }
+"""
+
+
+class TrainDataset(Dataset):
+
+    def __init__(self, args, raw_datasets, cache_root):
+        # This tab processor is for table truncation and linearize.
+        self.raw_datasets = raw_datasets
+
+        cache_path = os.path.join(cache_root, 'totto_context_aligning_train.cache')
+        if os.path.exists(cache_path) and args.dataset.use_cache:
+            self.extended_data = torch.load(cache_path)
+        else:
+            self.extended_data = []
+            expansion = args.seq2seq.expansion if args.seq2seq.expansion else 1
+            for expand_id in range(expansion):
+                for raw_data in tqdm(self.raw_datasets):
+                    extend_data = deepcopy(raw_data)
+                    statement = extend_data["statement"].lower()
+                    label_str = label_id2label_str[extend_data["label"]]
+                    linear_table = extend_data["table"]
+
+                    extend_data.update({"struct_in": linear_table.lower(),
+                                        "text_in": statement.lower(),
+                                        "seq_out": label_str.lower()})
+                    self.extended_data.append(extend_data)
+            if args.dataset.use_cache:
+                torch.save(self.extended_data, cache_path)
+
+    def __getitem__(self, index) -> T_co:
+        return self.extended_data[index]
+
+    def __len__(self):
+        return len(self.extended_data)
+
+
+class DevDataset(Dataset):
+
+    def __init__(self, args, raw_datasets, cache_root):
+        # This tab processor is for table truncation and linearize.
+        self.raw_datasets = raw_datasets
+
+        cache_path = os.path.join(cache_root, 'totto_context_aligning_dev.cache')
+        if os.path.exists(cache_path) and args.dataset.use_cache:
+            self.extended_data = torch.load(cache_path)
+        else:
+            self.extended_data = []
+            expansion = args.seq2seq.expansion if args.seq2seq.expansion else 1
+            for expand_id in range(expansion):
+                for raw_data in tqdm(self.raw_datasets):
+                    extend_data = deepcopy(raw_data)
+                    statement = extend_data["statement"].lower()
+                    label_str = label_id2label_str[extend_data["label"]]
+                    linear_table = extend_data["table"]
+
+                    extend_data.update({"struct_in": linear_table.lower(),
+                                        "text_in": statement.lower(),
+                                        "seq_out": label_str.lower()})
+                    self.extended_data.append(extend_data)
+            if args.dataset.use_cache:
+                torch.save(self.extended_data, cache_path)
+
+    def __getitem__(self, index) -> T_co:
+        return self.extended_data[index]
+
+    def __len__(self):
+        return len(self.extended_data)
---
+++ skr2/table/context_aligner/modeling/dataset_totto.py
@@ -0,0 +1,117 @@
+# coding=utf-8
+# Copyright 2021 The HuggingFace Datasets Authors, The Google AI Language Team Authors and the current dataset script contributor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The WikiTableQuestions dataset is for the task of question answering on semi-structured HTML tables"""
+# TODO： This code can be push to HuggingFace as a new contribution.
+import ast
+import csv
+import os
+import json
+
+import datasets
+
+# Find for instance the citation on arxiv or on the dataset repo/website
+_CITATION = """\
+"""
+
+_DESCRIPTION = """\
+"""
+
+_HOMEPAGE = ""
+
+_LICENSE = "CC-BY-SA-4.0 License"
+
+_URL = ""
+
+
+def _load_table(table_path) -> dict:
+    """
+    attention: the table_path must be the .tsv path.
+    Load the WikiTableQuestion from csv file. Result in a dict format like:
+    {"header": [header1, header2,...], "rows": [[row11, row12, ...], [row21,...]... [...rownm]]}
+    """
+
+    def __extract_content(_line: str):
+        _vals = [_.replace("\n", " ").strip() for _ in _line.strip("\n").split("\t")]
+        return _vals
+
+    with open(table_path, "r") as f:
+        lines = f.readlines()
+
+        rows = []
+        for i, line in enumerate(lines):
+            line = line.strip('\n')
+            if i == 0:
+                header = line.split("\t")
+            else:
+                rows.append(__extract_content(line))
+
+    table_item = {"header": header, "rows": rows}
+
+    # Defense assertion
+    for i in range(len(rows) - 1):
+        if not len(rows[i]) == len(rows[i - 1]):
+            raise ValueError('some rows have diff cols.')
+
+    return table_item
+
+
+class ToTToContextAligning(datasets.GeneratorBasedBuilder):
+    """The processed ToTTo dataset for Context Aligning dataset"""
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "statement": datasets.Value("string"),
+                    "table": datasets.Value("string"),
+                    "label": datasets.Value("int32"),
+                }
+            ),
+            supervised_keys=None,
+            homepage=_HOMEPAGE,
+            license=_LICENSE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        # data_dir = os.path.join(dl_manager.download_and_extract(_URL), 'WikiTableQuestions-master')
+        # Use local path for now
+        data_dir = "/home/p8shi/relogic-sql/data/examples/tablekit/ToTTo/totto_data/"
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"filepath": os.path.join(data_dir, "totto_train_data_alignment_classification.jsonl")},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.VALIDATION,
+                gen_kwargs={"filepath": os.path.join(data_dir, "totto_dev_data_alignment_classification.jsonl")},
+            )
+        ]
+
+    def _generate_examples(self, filepath):
+        """Yields examples."""
+        # data_id, question, table_id, gold_result_str
+        with open(filepath, encoding="utf-8") as f:
+            for idx, line in enumerate(f):
+                ex = json.loads(line)
+                yield idx, {
+                    "id": str(idx),
+                    "table": ex["table"],
+                    "statement": ex["text"],
+                    "label": ex["label"]
+                }
